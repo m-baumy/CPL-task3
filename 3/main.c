3285af98d8f562e72ee3f843fa87e1906731856c
m-baumy <81489715+m-baumy@users.noreply.github.com>
2021-11-07 08:52:10 +0200

    Add files via upload

--- 3/main.c
+++ 3/main.c
@@ -3,26 +3,26 @@ int main(){
  float x ;
  printf ("Enter the number to check: " "\n") ;
  scanf("%f" , &x) ;
- if (x>=0 && x<25)  { 
+ if (x>=0 && x<=25)  { 
   
     printf("%s" ,"Interval[0,25]") ;
 
  }
  
- else if ( x >= 25 && x < 50)  { 
+ else if ( x > 25 && x < 50)  { 
   
-    printf("%s" ,"Interval[25,50]") ;
+    printf("%s" ,"Interval(25,50)") ;
 
  }
   else if ( x >= 50 && x < 75)  { 
   
-    printf("%s" ,"Interval[50,75]") ;
+    printf("%s" ,"Interval[50,75)") ;
 
  }
  
   else if ( x >= 75 && x <100)  { 
   
-    printf("%s" ,"Interval[75,100]") ;
+    printf("%s" ,"Interval[75,100)") ;
 
  }
  else { 
